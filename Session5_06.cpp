0c2f87225e457c2f30b658014c39f7f5c4cf0d0d
hoanganholop <hoanganholop@gmail.com>
2024-11-25 17:31:01 +0700

    Add files via upload

---
+++ Session5_06.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+	printf("CALCULATOR\n");
+	for(int a; int b){
+		printf("Nhap so thu nhat:\n");
+		scanf("%d",&a);
+		printf("Nhap so thu hai:\n");
+		scanf("%d",&b);
+		printf("%d * %d = %d", a, b, a*b);
+		printf("%d * %d = %d", a, b, a*b);
+		printf("%d * %d = %d", a, b, a*b);
+		printf("%d * %d = %d", a, b, a*b);
+		
+	}
+}
